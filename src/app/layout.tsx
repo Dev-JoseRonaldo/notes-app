2bc953571a026e2459e6bc7b8c30fab253b74c12
José Ronaldo <dev.joseronaldo@gmail.com>
2024-02-11 15:29:00 -0300

    feat: add create new note toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "sonner";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${inter.className} bg-slate-900 text-slate-50 antialiased`}>{children}</body>
+      <body className={`${inter.className} bg-slate-900 text-slate-50 antialiased`}>
+        <Toaster richColors/>
+        {children}
+      </body>
     </html>
   );
 }
