6acfe6a438ca85c7c9f31e055c465df1571a2b1c
José Ronaldo <dev.joseronaldo@gmail.com>
2024-02-11 13:15:39 -0300

    feat: add home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,21 @@ import { NoteCard } from "@/components/NoteCard";
 export default function Home() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
         <NoteCard />
       </div>
     </div>
