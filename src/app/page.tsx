5abf93514bc7023b9ab3ef888bc4ae3172c16bdb
José Ronaldo <dev.joseronaldo@gmail.com>
2024-02-11 13:12:36 -0300

    feat: add NoteCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { NewNoteCard } from "@/components/NewNoteCard";
+import { NoteCard } from "@/components/NoteCard";
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
+        <NoteCard />
       </div>
     </div>
   );
